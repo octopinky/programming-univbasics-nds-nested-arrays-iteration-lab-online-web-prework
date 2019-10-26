6481d00660da487a41cb48c31f5b4f97f2e903bd
Olga  Chupp <olga.chupp@gmail.com>
2019-10-26 16:40:48 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -9,13 +9,9 @@ def join_ingredients(src)
 end
 
 def find_greater_pair(src)
-  results = []
-  
-  src.each do |ingredient_array|
-    results << "I love #{ingredient_array[0]} and #{ingredient_array[1]} on my pizza"
-  end
-  
-  results # implicitly return results
+  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
+  # Produce a new Array that contains the larger number of each of the pairs
+  # that are in the inner Arrays
 end
 
 def total_even_pairs(src)
